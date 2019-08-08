b967576f11d9ea1616aa8b1b4d724d0dff4a1317
zaralass <53153500+zaralass@users.noreply.github.com>
2019-08-08 21:26:08 +0200

    Added score counter
    Added score counter w/ basic bitch GUI and colouring to indicate the tagged player.

--- guten tag/Assets/scripts/tagging_control.cs
+++ guten tag/Assets/scripts/tagging_control.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class tagging_control : MonoBehaviour {
 
 	public float timer;
+	public float stimer = 1.0f;
+	public int score = 0;
 	[Tooltip("Controls who starts the round as tagged. 0 is random, 1 is player, above is AI.")]
 	public int InitTag = 0;
 	GameObject[] tagList = null;
-	tagging_client ptar;
+	GameObject tagObj;
+	GameObject player;
+	tagging_client tar;
+	int iTagged;
 	// Use this for initialization
 	void Start () {
 		// Create array of taggable entities if it doesn't exist
@@ -18,22 +23,17 @@ public class tagging_control : MonoBehaviour {
 		// retrieve player from array
 		for (int i = 0; i < tagList.Length; i++) {
 			if (tagList[i].name == "player") {
-				GameObject player = tagList[i];
-				ptar = player.GetComponent<tagging_client> ();
-				break;
+				player = tagList[i];
 			}
 		}
-		if (InitTag == 0) // random init tagger
-		{
-			int iTagged = Random.Range(0,tagList.Length);
-			tagging_client tar = tagList[iTagged].GetComponent<tagging_client> ();
-			tar.isTag = true;
+		iTagged = Random.Range(0,tagList.Length);
+		tagObj = tagList[iTagged];
+		if (InitTag == 1) { // char init tagger 
+			tagObj = player;
 		}
-		else if (InitTag == 1) // char init tagger
-			ptar.isTag = true;
-		else // enemy init tagger
-		{
-		}
+		tar = tagObj.GetComponent<tagging_client> ();
+		tar.isTag = true;
+		tagObj.GetComponent<SpriteRenderer>().color = Color.red;
 	}
 	
 	
@@ -44,8 +44,19 @@ public class tagging_control : MonoBehaviour {
 			//ticks timer if timer has value
 			//print (timer);
 		}
-
+		// Give score
+		if (stimer <= 0) {
+			if (tagObj != player) {
+				score++;
+				print("Score; " + score);
+			}
+			stimer = 1.0f;
+		}
+		stimer -= Time.deltaTime;
 	}
+	void OnGUI () {
+		GUI.Label (new Rect (20,20,220,120), "Score: " + score);
+    }
 	// tag function called by tagger collision
 	public void Tag (GameObject tagger, GameObject taggee)
 	{
@@ -53,6 +64,9 @@ public class tagging_control : MonoBehaviour {
 		timer = 2.0f;
 		tagger.GetComponent<tagging_client>().isTag = false;
 		taggee.GetComponent<tagging_client>().isTag = true;
+		tagger.GetComponent<SpriteRenderer>().color = Color.white;
+		taggee.GetComponent<SpriteRenderer>().color = Color.red;
+		tagObj = taggee;
 		print(tagger.name + " tagged " + taggee.name);
 	}
 }
